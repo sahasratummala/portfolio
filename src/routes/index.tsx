87c42ed50510219ac7ca9596e650cc91f2387e36
sahasratummala <76259284+sahasratummala@users.noreply.github.com>
2026-07-25 13:23:45 -0600

    kalarani shows up better

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -568,7 +568,17 @@ function BookshelfSection() {
 
         <article className="bookshelf-listening">
           <div className="bookshelf-card__cover bookshelf-card__cover--spotify">
-            <iframe data-testid="embed-iframe" src="https://open.spotify.com/embed/track/5ROXfVUT3pKjkan6nudSQh?utm_source=generator&si=ba9d69931a864c4a" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe
+              className="bookshelf-listening__embed"
+              data-testid="embed-iframe"
+              src="https://open.spotify.com/embed/track/5ROXfVUT3pKjkan6nudSQh?utm_source=generator&si=ba9d69931a864c4a"
+              title="Spotify track embed"
+              width="100%"
+              height="352"
+              frameBorder="0"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            />
           </div>
         </article>
       </div>
